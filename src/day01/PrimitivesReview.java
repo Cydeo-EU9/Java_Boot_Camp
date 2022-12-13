7bcd9df2328ef70334c5c524e9fc019f11ba0a17
Muhtar <muhtar@cybertekschool.com>
2022-12-13 10:28:37 -0500

    Java Boot Camp: day01

---
+++ src/day01/PrimitivesReview.java
@@ -0,0 +1,29 @@
+package day01;
+
+public class PrimitivesReview {
+
+    public static void main(String[] args) {
+
+        // Data-Type  var;
+
+        int age = 10;
+        double tax = 2.5;
+        char ch = '$';
+        boolean r = true;
+
+
+        int b = 100;
+        double c =  b; // implicit casting
+
+
+        double x = 20.5;
+        int y = (int) x;  // explicit casting
+
+
+
+
+
+
+    }
+
+}
