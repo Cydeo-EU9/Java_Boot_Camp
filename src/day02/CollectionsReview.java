74482084dff04aefc029c1da7ad6569e11f3a350
Muhtar <muhtar@cybertekschool.com>
2022-12-14 16:03:34 -0500

    Java Boot Camp: day02

---
+++ src/day02/CollectionsReview.java
@@ -0,0 +1,23 @@
+package day02;
+
+import java.util.*;
+
+public class CollectionsReview {
+
+    public static void main(String[] args) {
+
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new LinkedList<>();
+
+        List<Integer> l3 = new Vector<>();
+
+
+        new LinkedHashSet<>();
+
+
+
+
+    }
+
+
+}
